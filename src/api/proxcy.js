24cb9afa01465f2ef4534d3395aaa6eacd7b5974
Suresh V <32087861+iamsureshv@users.noreply.github.com>
2024-04-27 21:42:04 +0530

    Update proxcy.js

--- src/api/proxcy.js
+++ src/api/proxcy.js
@@ -5,7 +5,7 @@ module.exports= app =>{
         createProxyMiddleware({
           // target: process.env.BASE_KEY,
           // target: env.BASE_KEY,
-          target:  "https://ec2-13-232-200-64.ap-south-1.compute.amazonaws.com:9091",         
+          target:  "https://ec2-13-201-42-175.ap-south-1.compute.amazonaws.com:9091",         
           changeOrigin: true,
           secure: false, 
 
